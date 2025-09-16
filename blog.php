fd2a772eae7a0899c604ee1f06d831493719c8ea
Adobageedo <100688637+Adobageedo@users.noreply.github.com>
2025-09-16 17:34:16 +0200

    first

---
+++ blog.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="fr-FR" class="no-js">
+	<head>
+
+		<!-- ==============================================
+		Title and Meta Tags
+		=============================================== -->
+		<meta charset="utf-8">
+		<title>Article de Laurine Morigault-Coutant - Ostéopathe sur Cergy</title>
+		<meta name="description" content="Consultez les articles du cabinet d'ostéopathie de Laurine Morigault-Coutant sur Cergy et apprenez en plus sur la pratique d'un ostéopathe et le déroulement d'une séance.">		
+		<meta name="Keywords" CONTENT="articles osteopathie Cergy">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<meta property="og:url" content="https://www.osteocergy.com/blog.php" />	
+		<meta property="og:type" content="blog" />		
+		<meta property="og:title" content="Article de Laurine Morigault-Coutant - Ostéopathe sur Cergy" />
+		<meta property="og:description" content="Consultez les articles du cabinet d'ostéopathie de Laurine Morigault-Coutant sur Cergy et apprenez en plus sur la pratique d'un ostéopathe et le déroulement d'une séance." />		
+		<meta property="og:image" content="https://www.osteocergy.com/uploads/osteo-06.jpg" />
+		
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content="https://www.osteocergy.com/blog.php" />
+        <meta name="twitter:title" content="Article de Laurine Morigault-Coutant - Ostéopathe sur Cergy" />
+        <meta name="twitter:description" content="Consultez les articles du cabinet d'ostéopathie de Laurine Morigault-Coutant sur Cergy et apprenez en plus sur la pratique d'un ostéopathe et le déroulement d'une séance." />
+        <meta name="twitter:image" content="https://www.osteocergy.com/uploads/osteo-06.jpg" />		
+        
+        <!-- ==============================================
+        Canonical
+        =============================================== -->
+        <link rel="canonical" href="blog.php" />
+        
+		<!-- ==============================================
+		Favicons
+		=============================================== -->
+		<link rel="apple-touch-icon" href="assets/images/favicon-1.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicon-2.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon-3.png">
+		
+		<!-- ==============================================
+		CSS
+		=============================================== -->    
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/css/flexslider.css">
+        		<link rel="stylesheet" href="assets/css/styles-13.css">
+		<link rel="shortcut icon" href="assets/images/logos/logo-osteo_13.png">
+		
+		<!-- ==============================================
+		Fonts
+		=============================================== -->
+		<link href='https://fonts.googleapis.com/css?family=Karla' rel='stylesheet' type='text/css'>
+		<!-- ==============================================
+		JS
+		=============================================== -->
+			
+		<!--[if lt IE 9]>
+			<script src="assets/js/libs/respond.min.js"></script>
+		<![endif]-->
+		
+		<script type="text/javascript" src="assets/js/libs/modernizr.min.js"></script>
+
+		<link href="https://goo.gl/maps/j2a1LCkY5N8cVRXW8" rel="publisher" />
+        
+        <style>
+            .links-breadcrumb {font-size:11px;color:#6d6d6d;list-style:none;margin:0;padding:10px 3px;}
+            .links-breadcrumb li {display:inline-block;}
+            .links-breadcrumb li:after {content:' > ';color:#6d6d6d;margin:0px 5px;}
+            .links-breadcrumb li:last-child:after {display:none;content:'';}
+            .links-breadcrumb a {font-size:11px;color:#6d6d6d;text-decoration: underline;}
+        </style>
+	
+	</head>
+  
+	<body data-spy="scroll" data-target="#main-nav" data-offset="200">
+	
+		<!--=== PAGE PRELOADER ===-->
+		<div id="page-loader"><span class="page-loader-gif"></span></div>
+		
+	
+		<!-- ==============================================
+		MAIN NAV
+		=============================================== -->
+		<div id="main-nav" class="navbar navbar-fixed-top scrolled">
+			<div class="container">
+			
+				<div class="navbar-header">
+				
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+					
+					<!-- ======= LOGO ========-->
+					<a class="navbar-brand" href="index.html"><img src="assets/images/logos/logo-osteo_13.png" alt="Ostéopathe Cergy"/>Ostéo Cergy</a>
+				
+				</div>
+				
+				<div id="site-nav" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="sr-only">
+							<a href="index.html#home" class="scrollto">Accueil</a>
+						</li>
+						<li>
+							<a href="index.html#pratique" class="scrollto">La pratique</a>
+						</li>
+						<li>
+							<a href="index.html#l-osteopathe" class="scrollto">L'ostéopathe</a>
+						</li>
+						<li>
+							<a href="index.html#patients" class="scrollto">Les patients</a>
+						</li>
+						<li>
+							<a href="index.html#remboursement" class="scrollto">Remboursement</a>
+						</li>
+						<li>
+							<a href="index.html#contact" class="scrollto">Contact</a>
+						</li>
+						<li>
+							<a href="blog.php">Articles</a>
+						</li>						
+					</ul>
+				</div><!--End navbar-collapse -->
+				
+			</div><!--End container -->
+			
+		</div><!--End main-nav -->
+        
+		<!-- ==============================================
+		Article
+		=============================================== -->
+        <section id="article" class="add-padding" style="padding-bottom:20px;">
+		<div class="container">
+            <ul class="links-breadcrumb">
+                <li><a href="index.html">Accueil</a></li>
+                <li>Articles</li>
+            </ul>
+        	        	<div class="blog-post">
+            	<div class="row">
+                	<div class="col-sm-3">
+                        <a href="post/l-osteopathie-pour-les-nourrissons-a-cergy">
+                                                        <img src="uploads/thumbs/nourrisson-osteopathe.jpg" alt="ostéopathe bébé nourrisson" title="ostéopathe bébé nourrisson" class="img-responsive">
+                        </a>
+                    </div>
+                    <div class="col-sm-9">
+                        <a href="post/l-osteopathie-pour-les-nourrissons-a-cergy"><h2 style="margin-top:0;">L'ostéopathie pour les nourrissons à Cergy.</h2></a>
+                        <p>
+Laurine Morigault-Coutant, Ostéopathe à Cergy pour nourrissons, et ancien ostéopathe à la maternité de Saint-Cloud, explique l’intérêt de l'ostéopathie pour les nourrissons.Je viens d'avoir un bébé, devrais-je l'emmener chez l'ostéopat... <a href='post/l-osteopathie-pour-les-nourrissons-a-cergy' class='readmore'>Lire plus...</a></p>
+                    </div>
+                </div>
+            </div>
+			        	<div class="blog-post">
+            	<div class="row">
+                	<div class="col-sm-3">
+                        <a href="post/quels-sont-les-bienfaits-de-l-osteopathie-pendant-la-grossesse">
+                                                        <img src="uploads/thumbs/osteopathe%20femme%20enceinte.jpg" alt="ostéopathe grossesse enceinte" title="ostéopathe grossesse enceinte" class="img-responsive">
+                        </a>
+                    </div>
+                    <div class="col-sm-9">
+                        <a href="post/quels-sont-les-bienfaits-de-l-osteopathie-pendant-la-grossesse"><h2 style="margin-top:0;">Quels sont les bienfaits de l'ostéopathie pendant la grossesse?</h2></a>
+                        <p>Laurine Morigault-Coutant, Ostéopathe à Cergy pour femmes enceintes, et ancien ostéopathe à la maternité de Saint-Cloud, explique les bienfaits de l'ostéopathie lors de la grossesse.Pourquoi consulter un ostéopathe pendant ma grossesse?Toutes ... <a href='post/quels-sont-les-bienfaits-de-l-osteopathie-pendant-la-grossesse' class='readmore'>Lire plus...</a></p>
+                    </div>
+                </div>
+            </div>
+			        </div>
+        </section>
+		
+		
+		<!-- ==============================================
+		Me contacter
+		=============================================== -->
+		<section id="contact" class="add-padding" style="padding-top:20px;">
+		
+			<div class="container text-center">
+				
+				<div class="row">
+				
+					<div class="col-sm-6 col-md-5 text-center scrollimation fade-up d1">
+					
+						<h2 class="big-text">CONTACT</h2>
+						
+						<p class="lead"><p>Le cabinet est ouvert du <b>lundi au samedi de 9h à 20h</b>.</p><p>Voici l'adresse : <b>5 rue des vendanges prochaines 95800 Cergy</b>.&nbsp;</p><p>Consultations sur rendez-vous au 06 32 02 86 57.</p><p><i>NB : Le cabinet d'ostéopathie est au rez-de-chaussée du centre médical.</i></p><p><i>Vous pouvez stationner sur le parking situé derrière le centre médical.</i><br><br><br><br>
+</p></p>
+						
+						<p><center><div class="btn btn-color3"><a style="text-decoration: none; color: white;" href="tel:06 32 02 86 57"><i class="fa fa-phone-square"></i> 06 32 02 86 57</a></div></center></p>			
+                        						<p><center><div class="btn btn-color3"><a style="text-decoration: none; color: white;" href="https://zenrdv.com/fr_FR/d/beranger-dompeix-osteopathe-cergy-dompeix/" target="_blank"><i class="fa fa-calendar-check-o"></i> RDV en ligne</a></div></center></p>
+					    					
+						<p>&nbsp;</p>		
+					
+					</div>
+				
+					<div class="col-sm-6 col-md-offset-1 scrollimation fade-left d3">
+	
+						<img src="uploads/acces-cabinet-3245.jpg" class="img-responsive polaroid" alt="Accès ostéopathe Cergy">
+					
+					</div>
+					
+				</div>
+				
+			</div>
+		
+		</section>
+			
+		<!-- ==============================================
+		Carte
+		=============================================== -->
+		<section id="map">
+		
+			<div id="gmap"></div>
+		
+		</section>
+		
+		<!-- ==============================================
+		FOOTER
+		=============================================== -->	
+		
+		<footer id="main-footer" class="add-padding bg-color3 border-top-color2">
+		
+			<div class="container">
+			
+				<ul class="social-links text-center">
+					<li><a href="https://www.facebook.com/Ost%C3%A9opathe-Cergy-B%C3%A9ranger-Dompeix-738532692960542/?eid=ARA5JkvpG-ymhPTXSKV26TVfo11GjZKNkCZkQ4c4_cig1bMTXSYpEx-m-irg_b8Zp-XlagFimouoixAu"><i class="fa fa-facebook fa-fw"></i></a></li>
+					<li><a href="https://goo.gl/maps/j2a1LCkY5N8cVRXW8"><i class="fa fa-google-plus fa-fw"></i></a></li>
+				</ul>
+			
+				<p class="text-center">&copy; 2016 - 2025 - Laurine Morigault-Coutant</p>
+				<p class="text-center">Ostéopathe - Adresse : 5 Rue des Vendanges Prochaines, 95800 Cergy - Téléphone : 06 32 02 86 57</p>		
+				<p><center><div class="btn btn-color3"><a style="text-decoration: none; color: white;" href="tel:0632028657"><i class="fa fa-phone-square"></i> 06 32 02 86 57</a></div></center></p>
+                <div class="text-center">
+                    <p>Diplôme d'Ostéopathie de&nbsp;<a href="https://www.holistea.fr/">HOLISTÉA</a>.</p>                </div>			
+			</div>
+
+									<script type="application/ld+json">{
+    "@context": "http://schema.org",
+    "@type": "MedicalOrganization",
+    "name": "Laurine Morigault-Coutant",
+    "url": "https://www.osteocergy.com",
+
+    "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+33632028657",
+        "contactType": "Customer Service",
+        "contactOption": "",
+        "areaServed": "Cergy",
+        "availableLanguage": "French"
+    },
+    "address": {
+        "@type": "PostalAddress",
+        "addressCountry": "France",
+        "addressLocality": "Cergy",
+        "postalCode": "95800",
+        "streetAddress": "5 Rue des Vendanges Prochaines"
+    }
+}</script>
+			
+		</footer>
+		
+		<!-- ==============================================
+		SCRIPTS
+		=============================================== -->	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.9.1.min.js">\x3C/script>')</script>
+		
+		<script src="assets/js/libs/bootstrap.min.js"></script>
+		<script src="assets/js/jquery.scrollto.js"></script>
+		<script src="assets/js/waypoints.min.js"></script>
+		<script src="assets/js/jquery.masonry.min.js"></script>
+		<script src="assets/js/jquery.flexslider.min.js"></script>
+		<script src="assets/js/jquery.backstretch.min.js"></script>
+		<script src="assets/js/twitterFetcher_min.js"></script>
+		<script src="https://maps.google.com/maps/api/js?sensor=true&key=AIzaSyC1RgA-ywO6EqRt4KbQ7rQA1WOgZ892hDw"></script>
+		<script src="assets/js/contact.js"></script>
+		<script src="assets/js/scripts.js"></script>
+		
+		<script>
+		
+			var mapPoint = {
+					'lat': 49.0461,
+					'lng': 2.04418,
+					'zoom' : 15,
+					'infoText':'<p>Laurine Morigault-Coutant\
+								<p>5 Rue des Vendanges Prochaines\
+								<br/>95800\
+								<br/>Cergy</p>',
+					'linkText':'Consulter la carte',
+					'mapAddress':'5 Rue des Vendanges Prochaines, 95800 Cergy',
+					'icon': 'assets/images/pin-carte.png'
+				};
+			
+		</script>
+		
+	</body>
+	
+</html>
